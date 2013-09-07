efb659fba6b0f17570fbd8386beac07dbeedc877
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2013-09-07 21:05:30 +0530

    add remarks for darkproducer

--- darkserverweb/buildid/utils.py
+++ darkserverweb/buildid/utils.py
@@ -5,7 +5,8 @@ def get_darkproducer_instances(rdb):
         dp_status = {}
         dp_status['arch'] = darkproducer_key
         dp_status['status'] = rdb.get(darkproducer_key)
-        dp_status['remarks'] = ""
+        dp_status['remarks'] = rdb.get(
+                'darkproducer-id-'+ darproducer_key.split('-')[2])
 
         darkproducer_instances.append(dp_status)
 
